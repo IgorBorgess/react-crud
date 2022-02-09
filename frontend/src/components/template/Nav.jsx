9413eae53f2224c2e841b44c6f73f6f4948549b0
Igor Borges <igorborgeswrk@gmail.com>
2022-02-08 21:27:47 -0300

    [If applied] will build the Main, Nav and make some adjustments to App.

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,5 +3,12 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> {props.menuHome}
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> {props.menuUsers}
+            </a>
+        </nav>
     </aside>
