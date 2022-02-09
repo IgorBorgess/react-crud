cd6872151f0a6f083c3ebeee25539af52510d100
Igor Borges <igorborgeswrk@gmail.com>
2022-02-08 22:22:21 -0300

    [If applied] will apply changes to Logo, Nav, App and create Routes to implement the routes.

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,16 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
+import React from 'react'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
-import React from 'react'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav menuHome="Início" menuUsers="Usuários"/>
-        <Home />
-        <Footer />
-    </div>
+    <HashRouter>
+        <div className="app">
+            <Logo />
+            <Nav menuHome="Início" menuUsers="Usuários"/>
+            <Routes />
+            <Footer />
+        </div>
+    </HashRouter>
+    // Poderia utilizar BrowserRouter para evitar o hash na URL.
+    // Nesse projeto não faz muita diferença.
